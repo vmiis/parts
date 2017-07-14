debeb5bd96eb150b04c7276f4090c41d3c6f4dad
Zhiming Zhou <zzm.vmiis@gmail.com>
2017-07-14 12:20:05 +1000

    update

--- layout/main_layout_process.3.js
+++ layout/main_layout_process.3.js
@@ -71,6 +71,7 @@ var load_module=function(name){
 		else if($vm.app_config.panels[name].config_url!=undefined){
 			//this is for panels
 			var url=$vm.app_config.panels[name].config_url;
+			if(url[0]=='/') url=$vm.hosting_path+url;
 			url+='?_v='+$vm.version+$vm.reload;
 			url=$vm.url(url);
 			console.log('loading '+url)
